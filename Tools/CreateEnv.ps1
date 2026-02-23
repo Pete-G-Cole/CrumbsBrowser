4dbc2d2ec46059aa20a3b40182f7b11c6ba03ca7
Pete Cole <pcole@profundis.co.uk>
2026-02-23 18:14:25 +0000

    add default appsettings.json file

--- Tools/CreateEnv.ps1
+++ Tools/CreateEnv.ps1
@@ -5,9 +5,9 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Exit
 }
 
-[System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Url", "https://www.meadroid.com", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__0__name", "user", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__0__value", "[USERNAME]", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Url", "https://github.com/Pete-G-Cole", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__0__name", "os", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__0__value", "[OS]", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__1__name", "keyCode", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__1__value", "84", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("CRUMBS_NoUI", "true", [System.EnvironmentVariableTarget]::Machine)
