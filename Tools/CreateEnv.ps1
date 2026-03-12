8a1dfeff05495e3d5881aaa6b33986ca430af977
Pete Cole <pcole@profundis.co.uk>
2026-03-12 10:41:13 +0000

    Update with latest UI settings

--- Tools/CreateEnv.ps1
+++ Tools/CreateEnv.ps1
@@ -10,5 +10,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 [System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__0__value", "[OS]", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__1__name", "keyCode", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("CRUMBS_Startup__Arguments__1__value", "84", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("CRUMBS_NoUI", "true", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("CRUMBS_UI__ADDRESSBAR", "false", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("CRUMBS_UI__NAVIGATIONBUTTONS", "true", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("CRUMBS_UI__HOMEBUTTON", "false", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("CRUMBS_Security__RequireHttps", "true", [System.EnvironmentVariableTarget]::Machine)
